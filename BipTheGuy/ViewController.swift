bc83c01fb3870010ac10aa090c30764b84df093a
Connor Goodman <goodmaco@bc.edu>
2021-09-19 22:01:44 -0400

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Connor Goodman on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
